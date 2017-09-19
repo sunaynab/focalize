f9b6d45698e9a327f85bc87f1086cd6b59182299
sunaynab <sunaynabhikha@gmail.com>
2017-09-19 14:37:28 -0700

    Finish Backend Auth

---
+++ focalize_app/app/models/user.rb
@@ -0,0 +1,60 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :integer          not null, primary key
+#  username          :string           not null
+#  password_digest   :string           not null
+#  email             :string           not null
+#  session_token     :string           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  profile_image_url :string
+#
+
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    generate_unique_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+
+  private
+
+  def generate_unique_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    while User.find_by(session_token: self.session_token)
+      self.session_token = new_session_token
+    end
+    self.session_token
+  end
+
+end
