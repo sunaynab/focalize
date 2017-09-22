7225114ef3b9198eb03e1293061de9bc754a4fc9
sunaynab <sunaynabhikha@gmail.com>
2017-09-22 09:59:00 -0700

    Finish homepage styling

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="home">
-      <img src="http://res.cloudinary.com/daesquwob/image/upload/v1506050099/aldain-austria-316143_vd8mcv.jpg"></img>
       <span>Become inspired by a community of talented photographers. Share your talent and inspire others.</span>
       <Link to="/sign-up">Join the Community</Link>
     </div>
