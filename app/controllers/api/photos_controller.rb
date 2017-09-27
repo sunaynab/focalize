135b5316fa270bd442f8816b611b914249ce43f0
sunaynab <sunaynabhikha@gmail.com>
2017-09-27 15:13:20 -0700

    testing

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -15,7 +15,7 @@ class Api::PhotosController < ApplicationController
   end
 
   def create
-    @photo = Photo.new(user_id: current_user.id, image: photo_params[:image])
+    @photo = Photo.new(user_id: current_user.id)
     unless @photo.save
       render json: @photo.errors.full_messages
     end
