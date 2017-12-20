8cbded29b4ce766ae40b9239ff4f7a948bb3375f
sunaynab <sunaynabhikha@gmail.com>
2017-12-19 21:51:22 -0800

    Add favicon

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: follows
+#
+#  id                :integer          not null, primary key
+#  follower_id       :integer          not null
+#  user_following_id :integer          not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class Follow < ApplicationRecord
   validates :user_following_id, :follower_id, presence: true
   validates :user_following_id, uniqueness: {scope: :follower_id}
